5865b790d62e4a0e03f3b0288a02e7191e4bd7a6
Isabella Moak <isabellamartinem@gmail.com>
2019-05-26 13:49:52 -0500

    complete iteration 1

---
+++ Emojional/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojional
+//
+//  Created by Isabella Moak on 5/26/19.
+//  Copyright © 2019 Isabella Moak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
